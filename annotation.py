c8df3a233ac0e6aa6e492f78569436ccd220b28d
Thanh Tung <thanhtung.dao2110@gmail.com>
2020-05-19 15:56:45 +0700

    Change something

--- annotation.py
+++ annotation.py
@@ -6,6 +6,8 @@ import time
 import matplotlib.pyplot as plt
 
 def relative_box_size(path):
+    '''take the relative bounding box size in the image with the path
+    @path: path in the directory to the image'''
     relative = []
     root = ET.parse(path).getroot()
     w_img = int(root.find('size/width').text)
@@ -21,19 +23,20 @@ def relative_box_size(path):
         relative.append(temp)
     return relative
 
-# ----- MAIN -----
-start_time = time.time()
+def main():
+    path_list = glob.glob('Annotations_new/*.xml')
+    relative_list = []
+    for path in path_list:
+        relative_list.extend(relative_box_size(path))
+    print('Number of images: %s' % len(path_list))
+    print('Number of ships: %s' % len(relative_list))
 
-path_list = glob.glob('Annotations_new/*.xml')
-relative_list = []
-for path in path_list:
-    relative_list.extend(relative_box_size(path))
-print('Number of images: %s' % len(path_list))
-print('Number of ships: %s' % len(relative_list))
+    plt.hist(relative_list, bins = 100)
+    plt.gca().set(title='Frequency Histogram', ylabel='Frequency', xlabel = 'Relative bbox size');
+    plt.savefig('F:/viettel/relative_box_size_graph/Fequency_result.png')
 
-plt.hist(relative_list, bins = 100)
-plt.gca().set(title='Frequency Histogram', ylabel='Frequency', xlabel = 'Relative bbox size');
-plt.savefig('F:/viettel/relative_box_size_graph/Fequency_result.png')
-
-print("--- %.2f seconds ---" % (time.time() - start_time))
-print('--- COMPLETE ---')
+if __name__ == '__main__':
+    start_time = time.time()
+    main()
+    print("--- %.2f seconds ---" % (time.time() - start_time))
+    print('--- COMPLETE ---')
