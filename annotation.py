3ba0bca51f46a540e2aa2cd93564e41df76202d7
Thanh Tung <thanhtung.dao2110@gmail.com>
2020-05-19 15:38:15 +0700

    Fix code

--- annotation.py
+++ annotation.py
@@ -6,15 +6,19 @@ import time
 import matplotlib.pyplot as plt
 
 def relative_box_size(path):
+    relative = []
     root = ET.parse(path).getroot()
-    xmin = int(root.find('object/bndbox/xmin').text)
-    ymin = int(root.find('object/bndbox/ymin').text)
-    xmax = int(root.find('object/bndbox/xmax').text)
-    ymax = int(root.find('object/bndbox/ymax').text)
-
     w_img = int(root.find('size/width').text)
     h_img = int(root.find('size/height').text)
-    relative = math.sqrt((xmax-xmin) * (ymax-ymin)) / math.sqrt(w_img * h_img)
+
+    boxes = root.findall('object/bndbox')
+    for box in boxes:
+        xmin = int(box[0].text)
+        ymin = int(box[1].text)
+        xmax = int(box[2].text)
+        ymax = int(box[3].text)
+        temp = math.sqrt((xmax-xmin) * (ymax-ymin)) / math.sqrt(w_img * h_img)
+        relative.append(temp)
     return relative
 
 # ----- MAIN -----
@@ -23,8 +27,9 @@ start_time = time.time()
 path_list = glob.glob('Annotations_new/*.xml')
 relative_list = []
 for path in path_list:
-    relative_list.append(relative_box_size(path))
-print(len(path_list), len(relative_list))
+    relative_list.extend(relative_box_size(path))
+print('Number of images: %s' % len(path_list))
+print('Number of ships: %s' % len(relative_list))
 
 plt.hist(relative_list, bins = 100)
 plt.gca().set(title='Frequency Histogram', ylabel='Frequency', xlabel = 'Relative bbox size');
