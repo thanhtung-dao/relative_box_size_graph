aac66c1c47248ba0ddba4362a41732d7cc0e94e8
Thanh Tung <thanhtung.dao2110@gmail.com>
2020-05-20 03:37:34 +0700

    PEP 8 format

--- annotation.py
+++ annotation.py
@@ -1,42 +1,50 @@
-import numpy as np
 from xml.etree import ElementTree as ET
 import glob
 import math
 import time
 import matplotlib.pyplot as plt
 
+
 def relative_box_size(path):
-    '''take the relative bounding box size in the image with the path
-    @path: path in the directory to the image'''
+    """take the relative bounding box size in the image with the path
+    @path: path in the directory to the image"""
     relative = []
     root = ET.parse(path).getroot()
-    w_img = int(root.find('size/width').text)
-    h_img = int(root.find('size/height').text)
+    w_img = int(root.find("size/width").text)
+    h_img = int(root.find("size/height").text)
 
-    boxes = root.findall('object/bndbox')
+    boxes = root.findall("object/bndbox")
     for box in boxes:
         xmin = int(box[0].text)
         ymin = int(box[1].text)
         xmax = int(box[2].text)
         ymax = int(box[3].text)
-        temp = math.sqrt((xmax-xmin) * (ymax-ymin)) / math.sqrt(w_img * h_img)
+        temp = math.sqrt((xmax - xmin) * (ymax - ymin)) / math.sqrt(
+            w_img * h_img
+        )
         relative.append(temp)
     return relative
 
+
 def main():
-    path_list = glob.glob('Annotations_new/*.xml')
+    path_list = glob.glob("Annotations_new/*.xml")
     relative_list = []
     for path in path_list:
         relative_list.extend(relative_box_size(path))
-    print('Number of images: %s' % len(path_list))
-    print('Number of ships: %s' % len(relative_list))
+    print("Number of images: %s" % len(path_list))
+    print("Number of ships: %s" % len(relative_list))
 
-    plt.hist(relative_list, bins = 100)
-    plt.gca().set(title='Frequency Histogram', ylabel='Frequency', xlabel = 'Relative bbox size');
-    plt.savefig('F:/viettel/relative_box_size_graph/Fequency_result.png')
+    plt.hist(relative_list, bins=100)
+    plt.gca().set(
+        title="Frequency Histogram",
+        ylabel="Frequency",
+        xlabel="Relative bbox size",
+    )
+    plt.savefig("F:/viettel/relative_box_size_graph/Fequency_result.png")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     start_time = time.time()
     main()
     print("--- %.2f seconds ---" % (time.time() - start_time))
-    print('--- COMPLETE ---')
+    print("--- COMPLETE ---")
